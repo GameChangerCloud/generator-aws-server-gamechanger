67c79e69af0f8d05bba685b70da5cb63d07b1f97
Bertrand PINEL <bpinel@ippon.fr>
2020-11-24 17:01:53 +0100

    Add PostGis support and move away from rds-data for initialisation

--- generators/app/templates/initDatabase/initDatabase.js
+++ generators/app/templates/initDatabase/initDatabase.js
@@ -1,46 +1,79 @@
-const rdsdata = require("rds-data");
+// require the AWS SDK
+const AWS = require('aws-sdk');
+const rdsDataService = new AWS.RDSDataService()
 
-let beginTransactionParams = {
+const databaseConnect = {
   secretArn: process.env.SECRETARN,
   resourceArn: process.env.RESOURCEARN,
   database: process.env.DATABASE,
 }
 
+const postgisExt = [ "postgis", "fuzzystrmatch", "postgis_tiger_geocoder", "postgis_topology" ];
+
+
 <%-createTables%>
 
 <%-addConstraints%>
 
-async function transac(){
-  const db = new rdsdata.RDSDatabase(beginTransactionParams).getInstance();
-  let hasFailed = false
-  await db.transaction().then(async (transactionId) => {
-    for(let i = 0; i < queriesAdd.length; i++){
-      if(!hasFailed) {
-        await db.query(queriesAdd[i].text, queriesAdd[i].tableName, transactionId).then(r => console.log(r)).catch(err => {console.log(err); hasFailed = true})
-      }
+const addPostGisSupport = async () => {
+  let selectParams = {
+    secretArn: databaseConnect.secretArn,
+    resourceArn: databaseConnect.resourceArn,
+    sql: "SELECT * FROM pg_extension where extname LIKE '%postgis%'",
+    database: databaseConnect.database
+  }
+  console.log("Executing SQL Query "+selectParams.sql);
+  let dbResponse = await rdsDataService.executeStatement(selectParams).promise();
+  if (dbResponse.records.length == 0) {
+    console.log("Creating geographic extensions");
+    for(let i = 0; i < postgisExt.length; i++){
+      let createExtensionsParams = {
+        secretArn: databaseConnect.secretArn,
+        resourceArn: databaseConnect.resourceArn,
+        sql: "CREATE EXTENSION "+postgisExt[i],
+        database: databaseConnect.database
+      } 
+      await rdsDataService.executeStatement(createExtensionsParams).promise();
     }
-    for(let i = 0; i < queriesConstraint.length; i++){
-      if(!hasFailed) {
-        await db.query(queriesConstraint[i].text, queriesConstraint[i].tableName, transactionId).then(r => console.log(r)).catch(err => {console.log(err); hasFailed = true})
-      }
-    }
-    if(!hasFailed) {
-      await db.commit(transactionId).then(r => {console.log("commit"); return "Tables added"});
-    }
-    else {
-      console.log("Transaction rolled back")
-      return "Error, tables not added"
-    }
-  });
-
+  }else {
+    console.log("Geographic extensions already there");
+  }
 }
-  
+
+const graphQLTables = async () => {
+  console.log("Creating GraphQL schema tables");
+
+  for(let i = 0; i < queriesAdd.length; i++){
+    let createTableParams = {
+      secretArn: databaseConnect.secretArn,
+      resourceArn: databaseConnect.resourceArn,
+      sql: queriesAdd[i].text,
+      database: databaseConnect.database
+    }
+    console.log("Executing SQL command "+createTableParams.sql);
+    await rdsDataService.executeStatement(createTableParams).promise();
+  }
+  for(let i = 0; i < queriesConstraint.length; i++){
+    let createTableParams = {
+      secretArn: databaseConnect.secretArn,
+      resourceArn: databaseConnect.resourceArn,
+      sql: queriesConstraint[i].text,
+      database: databaseConnect.database
+    }
+    console.log("Executing SQL command "+createTableParams.sql);
+    await rdsDataService.executeStatement(createTableParams).promise();
+  }
+  return "All OK";
+}
 
 module.exports.initDatabase = () => {
-
-  transac().then(r => console.log("Done :" + r));
+  try {
+    await addPostGisSupport();
+    await graphQLTables();
+  }
+  catch(e) {
+    return "Init failed";
+  }
+  return "Init done !"
 
 }
-
-
-
