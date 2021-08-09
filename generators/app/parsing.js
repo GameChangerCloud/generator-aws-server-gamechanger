d3b3056d7838fae9594c956f9465e20e027116bc
Adrien Guard <56919132+Adrienguard@users.noreply.github.com>
2021-08-09 12:19:45 +0200

    fix manyToOne constraint

--- generators/app/parsing.js
+++ generators/app/parsing.js
@@ -1039,7 +1039,7 @@ const getRelations = (types, scalarTypeNames) => { // console.log(JSON.stringify
                             "noNull": true,
                             "isArray": false,
                             "foreignKey": true,
-                            "constraint": "FOREIGN KEY (\"Fk_"+rfield.name+"_"+utils.getSQLTableName(rfield.type)+"_id\") REFERENCES \"" + type.sqlTypeName + "\" (\"Pk_" + utils.getSQLTableName(rfield.type) + "_id\")"
+                            "constraint": "FOREIGN KEY (\"Fk_"+rfield.name+"_"+type.sqlTypeName+"_id\") REFERENCES \"" + type.sqlTypeName + "\" (\"Pk_" + type.sqlTypeName + "_id\")"
                             
                         }
                         delegatedField["delegated_field"]["state"] = true
