04a9ce85c17d7de6419868c47d2b8c252d972ad9
Adrien Guard <56919132+Adrienguard@users.noreply.github.com>
2021-12-05 17:51:14 +0100

    set up typeScript generators

---
+++ generators/app/scalars/manageScalars.d.ts
@@ -0,0 +1,5 @@
+export function getFieldCreate(type: any, name: any): string | null;
+export function getFieldName(scalar: any, name: any, type: any): string | null;
+export function isScalar(type: any): boolean;
+export function isBasicType(type: any): boolean;
+export function getScalarFieldInfo(currentType: any, typesNameArray: any): any;
---
+++ generators/app/utils.d.ts
@@ -0,0 +1,2 @@
+export function getSQLTableName(typeName: any): any;
+export function getSQLTableName(typeName: any): any;
